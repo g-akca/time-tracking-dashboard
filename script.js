49cda2a0af64d02314779a7a98b40d07b811759a
g-akca <gok.akca66@gmail.com>
2026-02-22 00:43:38 +0300

    Fetch data from data.json, update cards based on timeframe selected

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 const tabButtons = document.querySelectorAll("#nav-list button");
+const cards = document.querySelectorAll(".card-section");
+
+let data = [];
+
+async function loadData() {
+    const response = await fetch("data.json");
+    data = await response.json();
+}
+
+function updateUI(timeframe) {
+    cards.forEach((card, index) => {
+        const current = data[index].timeframes[timeframe].current;
+        const previous = data[index].timeframes[timeframe].previous;
+
+        const durationEl = card.querySelector(".task-duration");
+        const lastEl = card.querySelector(".task-last");
+        durationEl.textContent = `${current}hrs`;
+
+        if (timeframe == "daily") {
+            lastEl.textContent = `Yesterday - ${previous}hrs`;
+        }
+        else if (timeframe == "weekly") {
+            lastEl.textContent = `Last Week - ${previous}hrs`;
+        }
+        else {
+            lastEl.textContent = `Last Month - ${previous}hrs`;
+        }
+    });
+}
 
 function changeTimeframe(button) {
     tabButtons.forEach(btn => btn.classList.remove("active-tab"));
     button.classList.add("active-tab");
+
+    const timeframe = button.id;
+    updateUI(timeframe);
 }
+
+loadData();
 
 tabButtons.forEach(button => {
     button.addEventListener("click", (e) => changeTimeframe(e.currentTarget));
